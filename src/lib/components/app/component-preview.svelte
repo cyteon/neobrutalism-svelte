a62694008782c8cfdee98d189310e6cb26fd34e9
cyteon <cyteon@hackclub.app>
2025-06-23 20:26:49 +0200

    button example and stuff

---
+++ src/lib/components/app/component-preview.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let { code, component } = $props();
+    let showPreview = $state(true);
+</script>
+
+<div class="border border-border shadow-shadow flex sm:h-12 h-10 font-semibold mt-6">
+    <button 
+        class={`
+            h-full transition-all w-full px-2 py-1 border
+            ${showPreview ? "text-main-foreground text-main-foreground bg-main border-border" : ""} 
+        `}       
+        onclick={() => (showPreview = true)}
+    >
+        Preview
+    </button>
+
+    <button 
+        class={`
+            h-full transition-all w-full px-2 py-1 border
+            ${showPreview ? "" : "text-main-foreground text-main-foreground bg-main border-border"} 
+        `}
+        onclick={() => (showPreview = false)}
+    >
+        Code
+    </button>
+</div>
+
+{#if showPreview}
+    <div class="grid not-prose flex w-full items-center justify-center z-15 relative border-2 mb-5 min-h-[200px] border-border border-t-1 sm:px-10 px-5 sm:py-20 py-10 shadow-shadow bg-white">
+        {@render component()}
+    </div>
+{:else}
+    <pre class="bg-black border-2 border-border code text-sm max-h-[300px] overflow-auto p-4 not-prose text-white shadow-shadow">{code}</pre>
+{/if}
---
+++ src/lib/examples/button.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Button from "$lib/components/ui/button.svelte";
+</script>
+
+<Button onclick={() => alert("Button clicked!")}>
+    Click me!
+</Button>
